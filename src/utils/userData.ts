73f960a2e6cf60ec5e414360e0ceb4b20f135b92
Gabriel Werneck <gabrielwrnck@gmail.com>
2023-03-27 09:03:26 -0300

    Changed user data

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,6 +1,6 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
+  nameUser: "Gabriel Werneck",
+  githubUser: "Dukine",
+  linkedinUser: "gabrielwwerneck",
+  whatsappNumber: "+5511981682577",
 };
